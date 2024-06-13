73dbf63976b8c2787e9911720d914d3649657f35
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-13 17:19:27 -0300

    implemented cd command

--- src/main/java/edu/austral/ingsis/clifford/ChangeDirectoryCommand.java
+++ src/main/java/edu/austral/ingsis/clifford/ChangeDirectoryCommand.java
@@ -11,6 +11,69 @@ public class ChangeDirectoryCommand implements Command {
 
   @Override
   public String execute(List<String> options, List<String> arguments) {
-    return "";
+    if (!options.isEmpty()) {
+      throw new IllegalArgumentException("no options allowed");
+    }
+    if (arguments.size() != 1) {
+      throw new IllegalArgumentException("invalid arguments");
+    }
+    String argument = arguments.getFirst();
+    switch (argument) {
+      case "." -> { }
+      case ".." -> cli.currentDirectory = goToParentDirectory();
+      default -> {
+        if (argument.startsWith("/")) {
+          cli.currentDirectory = cli.root;
+          String[] route = argument.split("/");
+          navigateRoute(route);
+        } else {
+          String[] route = argument.split("/");
+          navigateRoute(route);
+        }
+      }
+    }
+    return getSuccessMessage(cli.currentDirectory.getName());
+  }
+
+  private String getSuccessMessage(String directoryName) {
+    return "moved to directory: '" + directoryName + "'";
+  }
+
+  private Directory goToParentDirectory() {
+    Directory parent = findDirectoryWithChild(cli.root, cli.currentDirectory.getName());
+    if (parent == null) {
+      throw new IllegalArgumentException("already at root");
+    }
+    return parent;
+  }
+
+  private Directory findDirectoryWithChild(Directory directory, String childName) {
+    List<FileSystem> children = directory.getChildren();
+    for (FileSystem child : children) {
+      if (!(child instanceof Directory)) {
+        continue;
+      }
+      if (child.getName().equals(childName)) {
+        return directory;
+      }
+      Directory search = findDirectoryWithChild((Directory) child, childName);
+      if (search != null) {
+        return search;
+      }
+    }
+    return null;
+  }
+
+  private void navigateRoute(String[] route) {
+    Directory startingDirectory = cli.currentDirectory;
+    for (String s : route) {
+      try {
+        FileSystem nextDirectory = cli.currentDirectory.getChild(s);
+        cli.currentDirectory = (Directory) nextDirectory;
+      } catch (Exception e) {
+        cli.currentDirectory = startingDirectory;
+        throw new IllegalArgumentException(s + " is not a directory");
+      }
+    }
   }
 }
