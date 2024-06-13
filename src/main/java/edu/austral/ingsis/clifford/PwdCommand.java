020624b9f25c0725c6e18f1b57849717595d78a7
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-06-13 18:28:33 -0300

    implemented pwd command

--- src/main/java/edu/austral/ingsis/clifford/PwdCommand.java
+++ src/main/java/edu/austral/ingsis/clifford/PwdCommand.java
@@ -11,6 +11,48 @@ public class PwdCommand implements Command {
 
   @Override
   public String execute(List<String> options, List<String> arguments) {
-    return "";
+    StringBuilder stringBuilder = new StringBuilder("/");
+    Directory index = cli.currentDirectory;
+    while (true) {
+      Directory parent = findDirectoryWithChild(cli.root, index.getName());
+      if (parent == null) {
+        break;
+      }
+      stringBuilder.insert(0, parent.getName());
+    }
+    stringBuilder.append(cli.currentDirectory.getName());
+    int rootNameLength = cli.root.getName().length();
+    stringBuilder.replace(0, rootNameLength, "/");
+    return stringBuilder.toString();
+  }
+
+  private Directory findDirectoryWithChild(Directory directory, String childName) {
+    List<FileSystem> children = directory.getChildren();
+    for (FileSystem child : children) {
+      if (!(child instanceof Directory)) {
+        continue;
+      }
+      if (child.getName().equals(childName)) {
+        return directory;
+      }
+      Directory search = findDirectoryWithChild((Directory) child, childName);
+      if (search != null) {
+        return search;
+      }
+    }
+    return null;
+  }
+
+  private void navigateRoute(String[] route) {
+    Directory startingDirectory = cli.currentDirectory;
+    for (String s : route) {
+      try {
+        FileSystem nextDirectory = cli.currentDirectory.getChild(s);
+        cli.currentDirectory = (Directory) nextDirectory;
+      } catch (Exception e) {
+        cli.currentDirectory = startingDirectory;
+        throw new IllegalArgumentException(s + " is not a directory");
+      }
+    }
   }
 }
